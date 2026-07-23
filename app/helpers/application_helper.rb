2077b553c424ab5228d73de07316f009fc7a7394
Aldrees7 <abdullah.idris777@gmail.com>
2026-07-23 16:01:19 +0300

    Polish styling and restore entry filters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def money(amount, currency)
+    "#{number_with_precision(amount || 0, precision: 2, delimiter: ',')} #{currency}"
+  end
+
+  def country_name(code)
+    {
+      "GB" => "United Kingdom",
+      "DE" => "Germany",
+      "FR" => "France",
+      "US" => "United States",
+      "CH" => "Switzerland"
+    }.fetch(code, code.presence || "Unassigned")
+  end
 end
